9f89665b4cb9372121ecdf25bfe76e13241916d5
pahagon <paulo.ahagon@gmail.com>
2024-06-03 12:12:43 -0300

    feat: Adds joystick

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,40 +1,205 @@
 #include <SoftwareSerial.h>
 
+#define DEBUG
+
 const uint8_t in1 = 3;
 const uint8_t in2 = 5;
 const uint8_t in3 = 9;
 const uint8_t in4 = 10;
 
-int forwards  = 1;
-int backwards = 0;
-int stop = 2;
-
 SoftwareSerial rxTx(0, 1); // RX, TX
 
-void move(int desired_direction) {
-  if (desired_direction == forwards) {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
+struct Direction {
+  int quadrant;
+  int speedEngineA;
+  int speedEngineB;
+};
 
-  } else if (desired_direction == backwards) {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
+void move(Direction* direction) {
+  if(direction->quadrant == 0) {
+    stopEngine();
+  } else if(direction->quadrant < 3) {
+    analogWrite(in2, direction->speedEngineA);
+    analogWrite(in1, 0);
+    analogWrite(in4, direction->speedEngineB);
+    analogWrite(in3, 0);
   } else {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
+    analogWrite(in2, 0);
+    analogWrite(in1, direction->speedEngineA);
+    analogWrite(in4, 0);
+    analogWrite(in3, direction->speedEngineB);
   }
+}
+
+void stopEngine() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+boolean isDigit(char c) {
+  return (c >= '0' && c <= '9');
+}
+
+boolean isDelimiter(char c) {
+  return (c == ',');
+}
+
+boolean isTokenValid(String token) {
+  return token.length() >= 3 && token.length() <=7 && token.indexOf(',') > 0;
+}
+
+void parseEndToken(String &input, int &index) {
+  if (index > input.length() - 1)
+    return;
+
+  int i = index;
+  char c = input.charAt(index);
+  while(isDigit(c) == false && index < input.length()) {
+    index += 1;
+    c = input.charAt(index);
+  }
+
+  if (i == index)
+    index += 1;
+}
+
+void parseNumber(String &input, String &token, String &acc, int &index) {
+  if (index > input.length() - 1)
+    return;
+
+  int i = index;
+  char c = input.charAt(index);
+  while(isDigit(c) && index < input.length()) {
+    token += c;
+    index += 1;
+    c = input.charAt(index);
+  }
+
+  if (i == index)
+    index += 1;
+}
+
+boolean parseDelemiter(String &input, String &token, String &acc, int &index) {
+  if (index > input.length() - 1)
+    return;
+
+  char c = input.charAt(index);
+  if( isDelimiter(c) )
+    token += c;
+
+  index += 1;
+}
+
+void parse(String &received, String &token, String &acc, int &index, int &countTokens) {
+  if (received.length() <= 0)
+    return;
+
+  if (index > received.length() - 1)
+    return;
+
+  parseNumber(received, token, acc, index);
+  parseDelemiter(received, token, acc, index);
+  parseNumber(received, token, acc, index);
+  parseEndToken(received, index);
+
+  if( isTokenValid(token) ) {
+    countTokens += 1;
+    acc += token;
+  }
+
+  if (index < received.length() - 1)
+    acc += '|';
+
+  token = "";
+  parse(received, token, acc, index, countTokens);
+}
+
+String* parseDirections(String received, int &countDirectionsReceived) {
+  String token = "";
+  String acc = "";
+  int index = 0;
+
+  parse(received, token, acc, index, countDirectionsReceived);
+  String* directions = new String[countDirectionsReceived];
+
+  #ifdef DEBUG
+  Serial.print("acc=(");
+  Serial.print(acc);
+  Serial.println(")");
+  #endif
+
+  int beginIndex = 0;
+  for(int i = 0; i < countDirectionsReceived; i++){
+    int delimiterIndex = acc.indexOf('|', beginIndex);
+    directions[i] = acc.substring(beginIndex, delimiterIndex);
+    beginIndex = delimiterIndex + 1;
+  }
+
+  return directions;
+}
+
+void parseDirection(String received, Direction* &direction) {
+  int endIndex = received.indexOf(',');
+  String first = received.substring(0, endIndex);
+  String second = received.substring(endIndex+1, received.length());
+
+  #ifdef DEBUG
+  Serial.print("first[");
+  Serial.print(first);
+  Serial.print("],second[");
+  Serial.print(second);
+  Serial.print("] ");
+  #endif
+
+  int speed = first.toInt();
+  int angle = second.toInt();
+
+  #ifdef DEBUG
+  Serial.print("speed[");
+  Serial.print(speed);
+  Serial.print("],angle[");
+  Serial.print(angle);
+  Serial.print("] ");
+  #endif
+
+  speed = map(speed, 0, 100, 0, 255); // Map the value to the range 0-255
+
+  if(angle < 90) {
+    direction->quadrant = 1;
+  } else if(angle < 180) {
+    direction->quadrant = 2;
+  } else if(angle < 270) {
+    direction->quadrant = 3;
+  } else if(angle < 360) {
+    direction->quadrant = 4;
+  }
+
+  if(direction->quadrant == 1 || direction->quadrant == 4) {
+    direction->speedEngineB = speed;
+    direction->speedEngineA = (speed * (1.0 - cos(angle * PI / 180.0) / 2.0));
+  } else {
+    direction->speedEngineB = (speed * (1.0 - cos(angle * PI / 180.0) / 2.0));
+    direction->speedEngineA = speed;
+  }
+
+  #ifdef DEBUG
+  Serial.print("directions[");
+  Serial.print(direction->quadrant);
+  Serial.print(",");
+  Serial.print(direction->speedEngineA);
+  Serial.print(",");
+  Serial.print(direction->speedEngineB);
+  Serial.println("]");
+  #endif
 }
 
 void setup() {
   Serial.begin(115200);
   rxTx.begin(115200); // ESP8266 baud rate
+
+  while (!Serial) {};
 
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -42,27 +207,42 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  move(stop);
+  stopEngine();
 }
 
 void loop() {
-  while (rxTx.available() <= 0) {
-    delay(500);
-    Serial.print(".");
+  if (rxTx.available() <= 0) {
+    return;
   }
 
-  String received = rxTx.readString();
-  if(received.toInt() == forwards) {
-    move(forwards);
-  } else if (received.toInt() == backwards) {
-    move(backwards);
-  } else {
-    move(stop);
+  String received = "";
+  while (rxTx.available()) {
+    received += rxTx.readStringUntil('\n');
   }
+  rxTx.flush();
 
-  Serial.println("");
-  Serial.println("Received: " + received);
+  //delay(1500);
+  //String received;
+  //received = "";
+  //received = "100";
+  //received = "1,1";
+  //received = "10,10";
+  //received = "10,011\t\t\n1,2 0,2&900,1 2";
 
-  delay(2500);
-  move(stop);
+  #ifdef DEBUG
+  Serial.print("r=(");
+  Serial.print(received);
+  Serial.print(")");
+  Serial.print(" ");
+  #endif
+
+  int numOfDirections = 0;
+  String* directions = parseDirections(received, numOfDirections);
+
+  for(int i = 0; i < numOfDirections; i++) {
+    Direction* direction = (Direction*) malloc(sizeof(Direction));
+    parseDirection(directions[i], direction);
+    move(direction);
+    free(direction);
+  }
 }
--- esp/esp.ino
+++ esp/esp.ino
@@ -16,12 +16,9 @@ const char* indexHtmlPage = R"=====(
   </head>
   <body>
     <h1>Robô Arthur e Thomas</h1>
-    <button id="forwards">Go forwards</button>
-    <button id="backwards">Go backwards</button>
+    <div><canvas id="canvas" name="game" ></canvas></div>
     <div id="response"></div>
     <script>
-      const forwards = document.getElementById('forwards');
-      const backwards = document.getElementById('backwards');
       const response = document.getElementById('response');
 
       let ws;
@@ -35,34 +32,17 @@ const char* indexHtmlPage = R"=====(
               startHeartbeat();
           };
           ws.onmessage = (event) => {
-              if (event.data === "pong") {
-                  console.log("Received pong from server.");
-              } else {
-                  response.innerText = `Server says: ${event.data}`;
-              }
+              response.innerText = `Server says: ${event.data}`;
           };
           ws.onclose = () => {
               console.log('Disconnected');
               stopHeartbeat();
               setTimeout(connect, 1000);  // Try to reconnect after 1 second
           };
+          ws.onerror = (error) => {
+              console.error('WebSocket error:', error);
+          };
       }
-
-      forwards.addEventListener('click', () => {
-          if (ws.readyState === WebSocket.OPEN) {
-              ws.send(1);
-          } else {
-              console.error("WebSocket connection is not open.");
-          }
-      });
-
-      backwards.addEventListener('click', () => {
-          if (ws.readyState === WebSocket.OPEN) {
-              ws.send(0);
-          } else {
-              console.error("WebSocket connection is not open.");
-          }
-      });
 
       function startHeartbeat() {
           pingInterval = setInterval(function() {
@@ -75,8 +55,131 @@ const char* indexHtmlPage = R"=====(
       function stopHeartbeat() {
           clearInterval(pingInterval);
       }
-      
+
       connect();
+
+      var canvas, ctx, width, height, radius, x_orig, y_orig, largura, altura;
+      function resize() {
+
+          largura = window.innerWidth
+              || document.documentElement.clientWidth
+              || document.body.clientWidth;
+          altura = window.innerHeight
+              || document.documentElement.clientHeight
+              || document.body.clientHeight;
+
+          if (largura > altura) {
+              width = largura * 0.3646;
+              radius = width * 0.2;
+              height = altura * 0.65;
+          }
+
+          if (altura > largura) {
+              width = largura;
+              radius = width * 0.23
+              height = largura;
+          }
+
+          ctx.canvas.width = width;
+          ctx.canvas.height = height;
+          background();
+          joystick(width / 2, height / 2);
+      }
+
+      function background() {
+          x_orig = width / 2;
+          y_orig = height / 2;
+          ctx.beginPath();
+          ctx.arc(x_orig, y_orig, radius + 20, 0, Math.PI * 2, true);
+          ctx.fillStyle = '#0e37cd';
+          ctx.fill();
+      }
+
+      function joystick(width, height) {
+          ctx.beginPath();
+          ctx.arc(width, height, radius, 0, Math.PI * 2, true);
+          ctx.fillStyle = '#ffffff';
+          ctx.fill();
+          ctx.strokeStyle = '#4464d9';
+          ctx.lineWidth = 8;
+          ctx.stroke();
+      }
+
+      window.addEventListener("load", () => {
+          (canvas = document.getElementById("canvas")),
+          (ctx = canvas.getContext("2d")),
+          resize(),
+          document.addEventListener("mousedown", startDrawing),
+          document.addEventListener("mouseup", stopDrawing),
+          document.addEventListener("mousemove", Draw),
+          document.addEventListener("touchstart", startDrawing),
+          document.addEventListener("touchend", stopDrawing),
+          document.addEventListener("touchcancel", stopDrawing),
+          document.addEventListener("touchmove", Draw),
+          window.addEventListener("resize", resize);
+      });
+
+      let coord = { x: 0, y: 0 };
+      let paint = !1;
+
+      function getPosition(event) {
+          e = window.event || e;
+          var mouse_x = e.clientX || e.touches[0].clientX;
+          var mouse_y = e.clientY || e.touches[0].clientY;
+          coord.x = mouse_x - canvas.offsetLeft;
+          coord.y = mouse_y - canvas.offsetTop;
+      }
+
+      function is_it_in_the_circle() {
+          var current_radius = Math.sqrt(Math.pow(coord.x - x_orig, 2) + Math.pow(coord.y - y_orig, 2));
+          if (radius >= current_radius) return true
+          else return false
+      }
+
+      function startDrawing(event) {
+          paint = true;
+          getPosition(event);
+          if (is_it_in_the_circle()) {
+              ctx.clearRect(0, 0, canvas.width, canvas.height);
+              background();
+              joystick(coord.x, coord.y);
+              Draw();
+          }
+      }
+
+      function stopDrawing() {
+          paint = false;
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          background();
+          joystick(width / 2, height / 2);
+          if (ws.readyState === WebSocket.OPEN) {
+              ws.send("0,0");
+          } else {
+              console.error("WebSocket connection is not open.");
+          }
+      }
+
+      function Draw(t) {
+          if (paint) {
+              var velocidade, angulo_graus, angulo_rad, x_abs, y_abs;
+              ctx.clearRect(0, 0, canvas.width, canvas.height), background();
+              angulo_rad = Math.atan2(coord.y - y_orig, coord.x - x_orig);
+
+              (angulo_graus = -1 == Math.sign(angulo_rad) ? Math.round((180 * -angulo_rad) / Math.PI)
+               : Math.round(360 - (180 * angulo_rad) / Math.PI)),
+              is_it_in_the_circle() ? (joystick(coord.x, coord.y), (x_abs = coord.x), (y_abs = coord.y))
+                  : joystick((x_abs = radius * Math.cos(angulo_rad) + x_orig), (y_abs = radius * Math.sin(angulo_rad) + y_orig)),
+              getPosition(t);
+
+              velocidade = Math.round((100 * Math.sqrt(Math.pow(x_abs - x_orig, 2) + Math.pow(y_abs - y_orig, 2))) / radius);
+
+              if (ws.readyState === WebSocket.OPEN) {
+                  ws.send(`${angulo_graus},${velocidade}`);
+              } else {
+                  console.error("WebSocket connection is not open.");
+              }
+          }
+      }
     </script>
   </body>
 </html>
@@ -101,6 +204,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
         webSocket.sendTXT(num, "pong");
       } else {
         Serial.println((const char*)payload);
+        Serial.flush();
         webSocket.sendTXT(num, payload);
       }
       break;
