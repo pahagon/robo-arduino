0c73ac532b416b700bf131f05b661d83468c91aa
pahagon <paulo.ahagon@gmail.com>
2024-06-26 22:19:49 -0300

    refac(arduino,esp): Byte array as protocoll between Arduino and ESP01

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -9,207 +9,6 @@ const uint8_t in4 = 10;
 
 SoftwareSerial rxTx(0, 1); // RX, TX
 
-struct Direction {
-  int quadrant;
-  int speedEngineA;
-  int speedEngineB;
-};
-
-void move(Direction* direction) {
-  if(direction->quadrant == 0) {
-    stopEngine();
-  } else if(direction->quadrant < 3) {
-    analogWrite(in2, direction->speedEngineA);
-    analogWrite(in1, 0);
-    analogWrite(in4, direction->speedEngineB);
-    analogWrite(in3, 0);
-  } else {
-    analogWrite(in2, 0);
-    analogWrite(in1, direction->speedEngineA);
-    analogWrite(in4, 0);
-    analogWrite(in3, direction->speedEngineB);
-  }
-}
-
-void stopEngine() {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}
-
-boolean isDigit(char c) {
-  return (c >= '0' && c <= '9');
-}
-
-boolean isDelimiter(char c) {
-  return (c == ',');
-}
-
-boolean isTokenValid(String token) {
-  return token.length() >= 3 && token.length() <=7 && token.indexOf(',') > 0;
-}
-
-void parseEndToken(String &input, int &index) {
-  if (index > input.length() - 1)
-    return;
-
-  int i = index;
-  char c = input.charAt(index);
-  while(isDigit(c) == false && index < input.length()) {
-    index += 1;
-    c = input.charAt(index);
-  }
-
-  if (i == index)
-    index += 1;
-}
-
-void parseNumber(String &input, String &token, String &acc, int &index) {
-  if (index > input.length() - 1)
-    return;
-
-  int i = index;
-  char c = input.charAt(index);
-  while(isDigit(c) && index < input.length()) {
-    token += c;
-    index += 1;
-    c = input.charAt(index);
-  }
-
-  if (i == index)
-    index += 1;
-}
-
-boolean parseDelemiter(String &input, String &token, String &acc, int &index) {
-  if (index > input.length() - 1)
-    return;
-
-  char c = input.charAt(index);
-  if( isDelimiter(c) )
-    token += c;
-
-  index += 1;
-}
-
-void parse(String &received, String &token, String &acc, int &index, int &countTokens) {
-  if (received.length() <= 0)
-    return;
-
-  if (index > received.length() - 1)
-    return;
-
-  parseNumber(received, token, acc, index);
-  parseDelemiter(received, token, acc, index);
-  parseNumber(received, token, acc, index);
-  parseEndToken(received, index);
-
-  if( isTokenValid(token) ) {
-    countTokens += 1;
-    acc += token;
-  }
-
-  if (index < received.length() - 1)
-    acc += '|';
-
-  token = "";
-  parse(received, token, acc, index, countTokens);
-}
-
-String* parseDirections(String received, int &countDirectionsReceived) {
-  String token = "";
-  String acc = "";
-  int index = 0;
-
-  parse(received, token, acc, index, countDirectionsReceived);
-  String* directions = new String[countDirectionsReceived];
-
-  #ifdef DEBUG
-  Serial.print(F("acc=("));
-  Serial.print(acc);
-  Serial.println(F(")"));
-  #endif
-
-  int beginIndex = 0;
-  for(int i = 0; i < countDirectionsReceived; i++){
-    int delimiterIndex = acc.indexOf('|', beginIndex);
-    directions[i] = acc.substring(beginIndex, delimiterIndex);
-    beginIndex = delimiterIndex + 1;
-  }
-
-  return directions;
-}
-
-void parseDirection(String received, Direction* &direction) {
-  int endIndex = received.indexOf(',');
-  String first = received.substring(0, endIndex);
-  String second = received.substring(endIndex+1, received.length());
-
-  #ifdef DEBUG
-  Serial.print(F("first["));
-  Serial.print(first);
-  Serial.print(F("],second["));
-  Serial.print(second);
-  Serial.print(F("] "));
-  #endif
-
-  int angle = first.toInt();
-  int speed = second.toInt();
-
-  #ifdef DEBUG
-  Serial.print(F("speed["));
-  Serial.print(speed);
-  Serial.print(F("],angle["));
-  Serial.print(angle);
-  Serial.print(F("] "));
-  #endif
-
-  speed = map(speed, 0, 100, 0, 255); // Map the value to the range 0-255
-
-  if(angle < 90) {
-    direction->quadrant = 1;
-  } else if(angle < 180) {
-    direction->quadrant = 2;
-  } else if(angle < 270) {
-    direction->quadrant = 3;
-  } else if(angle < 360) {
-    direction->quadrant = 4;
-  }
-
-  if(direction->quadrant == 1 || direction->quadrant == 4) {
-    direction->speedEngineB = speed;
-    direction->speedEngineA = (speed * (1.0 - cos(angle * PI / 180.0) / 2.0));
-  } else {
-    direction->speedEngineB = (speed * (1.0 - cos(angle * PI / 180.0) / 2.0));
-    direction->speedEngineA = speed;
-  }
-
-  #ifdef DEBUG
-  Serial.print(F("directions["));
-  Serial.print(direction->quadrant);
-  Serial.print(F(","));
-  Serial.print(direction->speedEngineA);
-  Serial.print(F(","));
-  Serial.print(direction->speedEngineB);
-  Serial.println(F("]"));
-  #endif
-}
-
-void itShouldReset(int &cicles) {
-  cicles += 1;
-  if (cicles >= 50) {
-    asm volatile(" jmp 0");
-    delay(500);
-    cicles = 0;
-  } else {
-    #ifdef DEBUG
-    Serial.print(F("numOfCicles["));
-    Serial.print(cicles);
-    Serial.println(F("]"));
-    #endif
-  }
-}
-
 void setup() {
   Serial.begin(115200);
   rxTx.begin(115200); // ESP8266 baud rate
@@ -222,38 +21,49 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  stopEngine();
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
-int numOfCicles = 0;
 void loop() {
   if (rxTx.available() <= 0) {
     return;
   }
 
-  itShouldReset(numOfCicles);
-
-  String received = "";
-  while (rxTx.available()) {
-    received += rxTx.readStringUntil('\n');
-  }
+  byte enginesSpeed[8];
+  rxTx.readBytes(enginesSpeed, 8);  // Lê os 4 bytes da serial
   rxTx.flush();
 
+  uint8_t leftEngineSpeedIn1 = enginesSpeed[0];
+  uint8_t leftEngineSpeedIn2 = enginesSpeed[1];
+  uint8_t rightEngineSpeedIn1 = enginesSpeed[2];
+  uint8_t rightEngineSpeedIn2 = enginesSpeed[3];
+
   #ifdef DEBUG
-  Serial.print(F("r=("));
-  Serial.print(received);
+  Serial.print(F("leftEngineSpeedIn1("));
+  Serial.print(leftEngineSpeedIn1);
+  Serial.print(F("),"));
+
+  Serial.print(F("leftEngineSpeedIn2("));
+  Serial.print(leftEngineSpeedIn2);
+  Serial.print(F("),"));
+
+  Serial.print(F("rightEngineSpeedIn1("));
+  Serial.print(rightEngineSpeedIn1);
+  Serial.print(F("),"));
+
+  Serial.print(F("rightEngineSpeedIn2("));
+  Serial.print(rightEngineSpeedIn2);
   Serial.print(F(")"));
-  Serial.print(F(" "));
+
+  Serial.println(F(""));
   #endif
 
-  int numOfDirections = 0;
-  String* directions = parseDirections(received, numOfDirections);
+  analogWrite(in1, leftEngineSpeedIn1);
+  analogWrite(in2, leftEngineSpeedIn2);
 
-  for(int i = 0; i < numOfDirections; i++) {
-    Direction* direction = (Direction*) malloc(sizeof(Direction));
-    parseDirection(directions[i], direction);
-    move(direction);
-    free(direction);
-  }
-  delete[] directions;
+  analogWrite(in3, rightEngineSpeedIn1);
+  analogWrite(in4, rightEngineSpeedIn2);
 }
--- esp/esp.ino
+++ esp/esp.ino
@@ -2,6 +2,8 @@
 #include <ESP8266WebServer.h>
 #include <WebSocketsServer.h>
 
+//#define DEBUG
+
 WebSocketsServer webSocket(8765);
 ESP8266WebServer server(80);
 
@@ -175,6 +177,7 @@ const char* indexHtmlPage = R"=====(
 
               if (ws.readyState === WebSocket.OPEN) {
                   ws.send(`${angulo_graus},${velocidade}`);
+                  console.log(`${angulo_graus},${velocidade}`);
               } else {
                   console.error("WebSocket connection is not open.");
               }
@@ -191,24 +194,109 @@ void handleRequest() {
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   switch(type) {
-    case WStype_DISCONNECTED:
-      break;
     case WStype_CONNECTED:
-      {
-        IPAddress ip = webSocket.remoteIP(num);
-        webSocket.sendTXT(num, "Hello from ESP8266");
-      }
+      webSocket.sendTXT(num, "Hello from ESP8266");
+      break;
+    case WStype_DISCONNECTED:
+      webSocket.sendTXT(num, "Bye from ESP8266");
+      break;
+    case WStype_BIN:
+      webSocket.sendTXT(num, payload);
       break;
     case WStype_TEXT:
       if (strcmp((const char*)payload, "ping") == 0) {
         webSocket.sendTXT(num, "pong");
       } else {
-        Serial.println((const char*)payload);
+        String angleAndSpeed = String((char*) payload);
+        int endIndex = angleAndSpeed.indexOf(',');
+        String first = angleAndSpeed.substring(0, endIndex);
+        String second = angleAndSpeed.substring(endIndex+1, angleAndSpeed.length());
+
+        uint8_t angle = first.toInt();
+        uint8_t speed = second.toInt();
+        speed = map(speed, 0, 100, 0, 255);
+        uint speedByAngle = (speed * (1.0 - cos(angle * PI / 180.0) / 2.0));
+
+        uint8_t leftEngineSpeedIn1  = 0;
+        uint8_t leftEngineSpeedIn2  = 0;
+        uint8_t rightEngineSpeedIn1 = 0;
+        uint8_t rightEngineSpeedIn2 = 0;
+
+        int quadrant = 0;
+        if(angle < 90) {
+          quadrant = 1;
+        } else if(angle < 180) {
+          quadrant = 2;
+        } else if(angle < 270) {
+          quadrant = 3;
+        } else {
+          quadrant = 4;
+        }
+
+        if (quadrant == 0) {
+          leftEngineSpeedIn1 = 0;
+          leftEngineSpeedIn2 = 0;
+
+          rightEngineSpeedIn1 = 0;
+          rightEngineSpeedIn2 = 0;
+        } else if(quadrant == 1) {
+          leftEngineSpeedIn1 = speedByAngle;
+          leftEngineSpeedIn2 = 0;
+
+          rightEngineSpeedIn1 = speed;
+          rightEngineSpeedIn2 = 0;
+        } else if(quadrant == 2) {
+          leftEngineSpeedIn1 = speed;
+          leftEngineSpeedIn2 = 0;
+
+          rightEngineSpeedIn1 = speedByAngle;
+          rightEngineSpeedIn2 = 0;
+        } else if(quadrant == 3) {
+          leftEngineSpeedIn1 = speed;
+          leftEngineSpeedIn2 = 0;
+
+          rightEngineSpeedIn1 = speedByAngle;
+          rightEngineSpeedIn2 = 0;
+        } else if(quadrant == 4) {
+          leftEngineSpeedIn1 = speedByAngle;
+          leftEngineSpeedIn2 = 0;
+
+          rightEngineSpeedIn1 = speed;
+          rightEngineSpeedIn2 = 0;
+        }
+
+        byte enginesSpeed[4] = {
+          leftEngineSpeedIn1, leftEngineSpeedIn2,
+          rightEngineSpeedIn1, rightEngineSpeedIn2
+        };
+
+        #ifdef DEBUG
+        Serial.print(F("leftEngineSpeedIn1("));
+        Serial.print(leftEngineSpeedIn1);
+        Serial.print(F(")"));
+
+        Serial.print(F("leftEngineSpeedIn2("));
+        Serial.print(leftEngineSpeedIn2);
+        Serial.print(F(")"));
+
+        Serial.print(F("rightEngineSpeedIn1("));
+        Serial.print(rightEngineSpeedIn1);
+        Serial.print(F(")"));
+
+        Serial.print(F("rightEngineSpeedIn2("));
+        Serial.print(rightEngineSpeedIn2);
+        Serial.print(F(")"));
+        #endif
+
+        Serial.write(enginesSpeed, 4);
         Serial.flush();
-        webSocket.sendTXT(num, payload);
+
+        angleAndSpeed +=
+          String(leftEngineSpeedIn1)  + "," + String(leftEngineSpeedIn2) + "," +
+          String(rightEngineSpeedIn2) + "," + String(rightEngineSpeedIn2);
+
+        webSocket.sendTXT(num, angleAndSpeed);
       }
-      break;
-    case WStype_BIN:
       break;
   }
 }
@@ -220,26 +308,36 @@ const IPAddress local_IP(192,168,15,14);
 const IPAddress gateway(192,168,15,1);
 const IPAddress subnet(255,255,255,0);
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-
-  if (WiFi.config(local_IP, gateway, subnet)) {
+void wiFiShouldConnect() {
+  if (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.config(local_IP, gateway, subnet) == 0) {
+      delay(10);
+      Serial.print(F("c"));
+    }
 
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
+        delay(500);
+        Serial.print(F("s"));
     }
   }
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {};
+
+  wiFiShouldConnect();
 
   server.on("/", handleRequest);
   server.begin();
 
-  webSocket.begin();
   webSocket.onEvent(webSocketEvent);
+  webSocket.begin();
 }
 
 void loop() {
+  wiFiShouldConnect();
   server.handleClient();
   webSocket.loop();
 }
